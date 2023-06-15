c2328e2e8cd7af1774fd05346180ce7a9cf5caad
bezgolovyponch <maxieventprag@gmail.com>
2023-06-15 20:48:01 +0200

    initial

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
+// import React from 'react';
 
-import InputWithValidation from './InputValidation';
+// import InputWithValidation from './InputValidation';
 
-function Form() {
-  function handleSubmit(event) {
-    event.preventDefault();
-    // Do something with the form data
-  }
+// function Form() {
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     // Do something with the form data
+//   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <InputWithValidation
-        id="name"
-        name="name"
-        label="Name"
-        required
-        maxLength={50}
-      />
-      <InputWithValidation
-        id="email"
-        name="email"
-        label="Email"
-        type="email"
-        required
-        maxLength={100}
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <InputWithValidation
+//         id="name"
+//         name="name"
+//         label="Name"
+//         required
+//         maxLength={50}
+//       />
+//       <InputWithValidation
+//         id="email"
+//         name="email"
+//         label="Email"
+//         type="email"
+//         required
+//         maxLength={100}
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
 
-export default Form;
+// export default Form;
